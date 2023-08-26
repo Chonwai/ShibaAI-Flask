d23515fae9bb4784d38e5e335183e44d2e4b624e
Chonwai <chonwaiun@gmail.com>
2023-08-26 23:38:34 +0800

    Update: Updated the chatbot parameters.

--- api/services/qabot/qabot.py
+++ api/services/qabot/qabot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 import os
+import datetime
 load_dotenv()
 
 
@@ -25,14 +26,23 @@ class QABotService:
             embedding_function=QABotService.embeddings,
         )
 
-        docs = store.similarity_search(query, filter=filter, k=10)
+        # docs = store.similarity_search(query, filter=filter, k=10)
+
+        docs = store.as_retriever(
+            search_kwargs={'filter': filter, 'k': 10,
+                           'fetch_k': 20}
+        )
 
         print(docs)
 
-        query = query + "（包括活動地點以及活動開始和結束日期）"
+        query = query + "（根據用戶提供的描述，給用戶推薦兩個有效的活動，提問日期：{query_date}；推薦的兩個活動內容要包含活動地點以及活動開始和結束日期。）".format(
+            query_date=datetime.now().strftime("%Y-%m-%d")
+        )
+
+        print(query)
 
         chain = load_qa_chain(ChatOpenAI(model_name=os.getenv(
-            "OPENAI_MODEL_NAME"), temperature=0.7), chain_type="stuff")
+            "OPENAI_MODEL_NAME"), temperature=0.5), chain_type="stuff")
         res = chain({"input_documents": docs, "question": query},
                     return_only_outputs=True)
 
